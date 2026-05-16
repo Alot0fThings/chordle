ca0c6c17e09d690a86f50ab92823975ee6bb51c7
Joe Yang <JoeYang82@gmail.com>
2026-05-15 21:02:05 -0500

    fix github pages base path for real

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p146/',
+  base: '/chordle/', // making this change explicit brh
   plugins: [react()],
   build: {
     outDir: "docs"
