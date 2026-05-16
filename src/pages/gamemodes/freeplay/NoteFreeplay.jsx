5283b0e0cfd483c0da174836fd8b1642b76dd2d1
Joe Yang <JoeYang82@gmail.com>
2026-05-15 21:23:09 -0500

    update

--- src/pages/gamemodes/freeplay/NoteFreeplay.jsx
+++ src/pages/gamemodes/freeplay/NoteFreeplay.jsx
@@ -52,7 +52,7 @@ export default function NoteFreeplay(props) {
     return <div style={{ textAlign: "center" }}>
         <h1>Freeplay</h1>
         <Image
-            src={`/p146/notes/${note.name.replaceAll("#", "sharp")}.svg`}
+            src={`/chordle/notes/${note.name.replaceAll("#", "sharp")}.svg`}
             alt={note.description}
             style={{ height: 400, width: 400 }} />
 
